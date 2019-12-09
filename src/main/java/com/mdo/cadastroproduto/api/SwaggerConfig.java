96078c0f7716da5ab73399f83b3dff4b9cb295e9
magson <magsond@gmail.com>
2019-12-08 23:18:37 -0300

    api swegger

---
+++ src/main/java/com/mdo/cadastroproduto/api/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.mdo.cadastroproduto.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+ 
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+ 
+	@Bean
+	public Docket detalheApi() {
+ 
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
+ 
+		docket
+		.select()
+		.apis(RequestHandlerSelectors.basePackage("com.mdo.cadastroproduto"))
+		.paths(PathSelectors.any())
+		.build();
+		//.apiInfo(this.informacoesApi().build());
+ 
+		return docket;
+	}
+ 
+//	private ApiInfoBuilder informacoesApi() {
+// 
+//		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+// 
+//		apiInfoBuilder.title("Api-Pessoa");
+//		apiInfoBuilder.description("Api para realização de um CRUD.");
+//		apiInfoBuilder.version("1.0");
+//		apiInfoBuilder.termsOfServiceUrl("Termo de uso: Deve ser usada para estudos.");
+//		apiInfoBuilder.license("Licença - Open Source");
+//		apiInfoBuilder.licenseUrl("http://www.ciceroednilson.com.br");
+//		apiInfoBuilder.contact(this.contato());
+// 
+//		return apiInfoBuilder;
+// 
+//	}
+//	private Contact contato() {
+// 
+//		return new Contact(
+//				"Cícero Ednilson",
+//				"http://www.ciceroednilson.com.br", 
+//				"ciceroednilson@gmail.com");
+//	}
+}
